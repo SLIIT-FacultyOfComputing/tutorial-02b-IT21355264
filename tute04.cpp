9c4c943984cb2d9124f676f33fcb1bc57818e534
Hansi <99313529+IT21355264@users.noreply.github.com>
2022-02-17 11:49:00 +0000

    complete 04

--- tute04.cpp
+++ tute04.cpp
@@ -13,7 +13,7 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
+long Factorial(int n);
 long nCr(int n, int r);
 
 int main() {
@@ -26,4 +26,24 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+//implement functins
+long Factorial(int no)
+{
+  int r;
+  long fac = 1;
+  
+  for(r = no ; r >= 1 ; r--)
+    {
+      fac = fac * r;
+    }
+  
+  return fac; 
+}
+
+long nCr(int n , int r)
+{
+  long nCr = Factorial(n) / (Factorial(r) * Factorial(n-r));
+
+  return nCr;
 }
